47e109d13550eb1e778773f0996a1a8b8c61f173
Gaston Gallegos <ggallegos@mobydigital.com>
2023-07-23 20:23:14 -0300

    reactive errors
    errors appear dinamycally when the users enter an incorrect value and leaves the form field

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+}
+
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
@@ -20,8 +26,33 @@ export class BasicPageComponent {
     inStorage: [0, [ Validators.required, Validators.min(0) ]],
   })
   
-  constructor(private fb: FormBuilder){}
+  constructor( private fb: FormBuilder ){}
 
+  ngOnInit(): void {
+    //  this.myForm.reset( rtx5090 );
+  }
+
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null{
+    if ( !this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ){
+        
+        case 'required': 
+        return  'Este campo es requerido';
+
+        case 'minlength': 
+        return  `Minimo ${errors ['minlength'].requiredLength } caracteres.`;
+      }
+    }
+    return null;
+  }
 
   onSave():void{
     if (this.myForm.invalid) return;
